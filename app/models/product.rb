0ad0ac431105bc0d306a3ef6ddcf8d09276a4ef0
yoichi ogo <o541.1980@gmail.com>
2021-10-14 12:18:18 +0900

    documents#indexに対象製品のstatus検索を追加

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,5 +3,5 @@ class Product < ApplicationRecord
 
   enum category: {'マウス': 1, 'キーボード': 2, 'バッテリー': 3}
   enum status: {'開発中': 0, '製造中': 1, '製造中止': 2}
-  enum location: {'未定': 0, '東京': 1, '深圳': 2, 'バンコク': 2, 'デュッセルドルフ': 3, 'ダラス': 4}
+  enum location: {'未定': 0, '東京': 1, '深圳': 2, 'バンコク': 3, 'デュッセルドルフ': 4, 'ダラス': 5}
 end
