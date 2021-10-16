f25df608dd55c3f721ee95c5857f137ca4b42bf1
yoichi ogo <o541.1980@gmail.com>
2021-10-16 14:31:36 +0900

    User-ProjectUser-Project間のアソシエーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ApplicationRecord
   has_many :members, dependent: :destroy
   has_many :member_departments, through: :members, source: :department
 
+  has_many :project_users, dependent: :destroy
+  has_many :project_user_projects, through: :project_users, source: :project
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
