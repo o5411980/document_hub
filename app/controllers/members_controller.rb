c9f9a5fcd0ab5849671710e59f7389e253ae0b79
yoichi ogo <o541.1980@gmail.com>
2021-10-16 15:25:57 +0900

    project#showからparamsを出してproject_users#createで書込み処理実装

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,7 +1,7 @@
 class MembersController < ApplicationController
   def create
     member = User.find(params[:user][1]).members.create(department_id: params[:user][4])
-    redirect_to department_path(params[:user][1]), notice: "#{member.user.name}さんを#{member.department.name}に加えました"
+    redirect_to department_path(params[:user][4]), notice: "#{member.user.name}さんを#{member.department.name}に加えました"
   end
 
   def destroy
--- app/controllers/project_users_controller.rb
+++ app/controllers/project_users_controller.rb
@@ -1,7 +1,8 @@
 class ProjectUsersController < ApplicationController
   def create
-    member = User.find(params[:user][1]).members.create(department_id: params[:user][4])
-    redirect_to project_path(params[:user][1]), notice: "#{member.user.name}さんを#{member.project.name}プロジェクトに加えました"
+    byebug
+    member = User.find(params[:user][1]).project_users.create(project_id: params[:user][4])
+    redirect_to project_path(params[:user][4]), notice: "#{member.user.name}さんを#{member.project.name}に加えました"
   end
 
   def destroy
