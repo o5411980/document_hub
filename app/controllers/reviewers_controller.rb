b46f54aff604c1177b86e012d9739671570edfeb
yoichi ogo <o541.1980@gmail.com>
2021-10-16 16:36:14 +0900

    reviews#showからparamsを送って、reviewerのアサイン機能を実装

--- app/controllers/reviewers_controller.rb
+++ app/controllers/reviewers_controller.rb
@@ -1,6 +1,6 @@
 class ReviewersController < ApplicationController
   def create
-    reviewer = User.find(params[:user][1]).reviewers.create(project_id: params[:user][4])
+    reviewer = User.find(params[:user][1]).reviewers.create(review_id: params[:user][4])
     redirect_to review_path(params[:user][4]), notice: "#{reviewer.user.name}さんをレビュー担当者にしました"
   end
 
