01f2c0a1fa6c13221b7bbf485b87b5fd1bafa817
yoichi ogo <o541.1980@gmail.com>
2021-10-18 10:39:19 +0900

    Document-ReviewDocument-Review間のアソシエーション設定

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,6 +2,9 @@ class Document < ApplicationRecord
   mount_uploader :filepath, DocumentUploader
   belongs_to :product
 
+  has_many :review_documents, dependent: :destroy
+  has_many :review_document_reviews, through: :review_documents, source: :review
+
   enum category: {'その他文書': 0, 'ビジネスプラン': 1, 'マスタープラン': 2, '製品仕様書': 3, '設計書': 4, '試作報告書': 5, '量産試作報告書': 6, '歩留り評価報告書': 7, '作業手順書': 8, '議事録': 9 }
   enum authorize: {'未承認': false, '承認済': true}
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -5,6 +5,9 @@ class Review < ApplicationRecord
   has_many :reviewers, dependent: :destroy
   has_many :reviewer_users, through: :reviewers, source: :user
 
+  has_many :review_documents, dependent: :destroy
+  has_many :review_document_documents, through: :reviewers, source: :document
+
   enum stage: {'Review 1': 1, 'Review 2': 2, 'Review 3': 3}
   enum judge: {'未承認': false, '承認済': true}
 end
