0c18c75b5f16ac000a39c603479bde2078e2119b
yoichi ogo <o541.1980@gmail.com>
2021-10-20 11:37:23 +0900

    product#showページに該当製品のreview表示 issue#61

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+######使用時はL12より下の行の#を削除して、% rails db:seed
 i = 0
 locations = ["東京", "深圳", "バンコク", "デュッセルドルフ", "ダラス", "その他"]
 department_name = ["所属未定", "管理課", "企画課", "営業課", "製造課", "開発課", "その他"]
