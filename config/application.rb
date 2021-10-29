3a77a9f894272cf2dee67cea93dd6ff48e9a7c07
yoichi ogo <o541.1980@gmail.com>
2021-10-29 13:23:19 +0900

    validationのエラー文を日本語化

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module TechDocumentHub
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    config.i18n.available_locales = %i[ja en]
+    config.i18n.available_locales = %i[ja]
     config.i18n.default_locale = :ja
 
     config.generators do |g|
