e2929497511b2216400c1b711334409756d444ff
yoichi ogo <o541.1980@gmail.com>
2021-10-30 01:53:37 +0900

    spec/system/department_spec.rbを実装

--- spec/system/department_spec.rb
+++ spec/system/department_spec.rb
@@ -15,7 +15,51 @@ RSpec.describe '2,department CRUDのテスト', type: :system do
         fill_in '部署の名称', with: 'TEST'
         select '東京', from: '所在地'
         click_on '登録する'
-        expect(page).to have_content 'xxxx'
+        sleep 0.1
+        expect(page).to have_content '部署を作成しました。'
+      end
+    end
+  end
+
+  describe '2-2,departmentの更新テスト' do
+    before do
+      Department.destroy_all
+      Department.create!(name: 'TEST', location: '東京')
+    end
+    context '部署情報の編集画面で必要事項を記入すると、' do
+      it "部署情報が更新できること。" do
+        visit root_path
+        click_on 'ゲストログイン(管理者)'
+        sleep 0.1
+        click_on '部署一覧'
+        sleep 0.1
+        click_on '編集'
+        sleep 0.1
+        fill_in '部署の概要', with: '更新テスト'
+        click_on '更新する'
+        sleep 0.1
+        expect(page).to have_content '部署情報を更新しました。'
+      end
+    end
+  end
+
+  describe '2-3,departmentの削除テスト' do
+    before do
+      Department.destroy_all
+      Department.create!(name: 'TEST', location: '東京')
+    end
+    context '部署情報の編集画面で必要事項を記入すると、' do
+      it "部署情報が更新できること。" do
+        visit root_path
+        click_on 'ゲストログイン(管理者)'
+        sleep 0.1
+        click_on '部署一覧'
+        sleep 0.1
+        click_on '削除'
+        sleep 0.1
+        expect(page.accept_confirm).to eq "本当に削除しますか？"
+        sleep 0.1
+        expect(page).to have_content '部署を削除しました。'
       end
     end
   end
